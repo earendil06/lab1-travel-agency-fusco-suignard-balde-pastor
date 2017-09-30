bc3296733ba48c1fc854c15e761102e271840767
afusco <anthony.fusco42@gmail.com>
2017-09-30 17:21:54 +0200

    document all working

--- services/document/src/main/java/flights/Flight.java
+++ services/document/src/main/java/flights/Flight.java
@@ -22,21 +22,21 @@ public class Flight {
     public Flight(JSONObject data) {
         this.from = data.getString("from");
         this.to = data.getString("to");
-        this.dateFrom = (Date)data.get("dateFrom");
-        this.dateTo = (Date)data.get("dateTo");
-        this.price = data.getDouble("price");
-        this.directFlight = data.getBoolean("directFlight");
+//        this.dateFrom = (Date)data.get("dateFrom");
+//        this.dateTo = (Date)data.get("dateTo");
+//        this.price = data.getDouble("price");
+//        this.directFlight = data.getBoolean("directFlight");
     }
 
 
     JSONObject toJson() {
         return new JSONObject()
                 .put("from", this.from)
-                .put("to", this.to)
-                .put("dateFrom", this.dateFrom)
+                .put("to", this.to);
+/*                .put("dateFrom", this.dateFrom)
                 .put("dateTo", this.dateTo)
                 .put("price", this.price)
-                .put("directFlight", this.directFlight);
+                .put("directFlight", this.directFlight);*/
     }
 
 }
