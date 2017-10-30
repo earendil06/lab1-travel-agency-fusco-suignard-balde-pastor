0cf2ffbfe1e634b6a0d0ef892ff870b4645a2184
afusco <anthony.fusco42@gmail.com>
2017-10-30 13:57:42 +0100

    change merge expenseReport

--- integration/flows/src/main/java/fr/unice/groupe4/flows/ProcessSupportingRequest.java
+++ integration/flows/src/main/java/fr/unice/groupe4/flows/ProcessSupportingRequest.java
@@ -1,18 +1,17 @@
 package fr.unice.groupe4.flows;
 
-import com.google.gson.Gson;
+import fr.unice.groupe4.flows.data.ExpenseRequest;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import fr.unice.groupe4.flows.data.SupportingRequest;
-import fr.unice.groupe4.flows.data.ExpenseRequest;
 
 import static fr.unice.groupe4.flows.utils.Endpoints.*;
 
@@ -62,97 +61,23 @@ public class ProcessSupportingRequest extends RouteBuilder{
 
     }
 
-
     private static AggregationStrategy mergeExpense = new AggregationStrategy() {
         @Override
         public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
             Message newIn = newExchange.getIn();
             if (oldExchange == null) {
-                ExpenseRequest expenseRequest = new ExpenseRequest();
-                add(expenseRequest, newIn);
-                newIn.setBody(expenseRequest);
+                ExpenseRequest exp = newExchange.getIn().getBody(ExpenseRequest.class);
+                List<ExpenseRequest> list = new ArrayList<>();
+                list.add(exp);
+                newIn.setBody(list);
                 return newExchange;
             } else {
-                ExpenseRequest old = oldExchange.getIn().getBody(ExpenseRequest.class);
-                add(old, newIn);
-                oldExchange.getIn().setBody(old);
-                //System.out.println("MERGED " + old);
-                if (old != null &&
-                        old.getType() != null &&
-                        old.getMotif() != null &&
-                        old.getPrice() != null) {
-                    //old.getFlight() != null) {
-                    System.out.println("EVERYTHING Expenses IS AGGREGATED\n" + old);
-                }
+                List<ExpenseRequest> oldList = oldExchange.getIn().getBody(List.class);
+                ExpenseRequest exp = newExchange.getIn().getBody(ExpenseRequest.class);
+                oldList.add(exp);
                 return oldExchange;
-            }
-        }
-
-        private void add(ExpenseRequest old, Object o) {
-            Message msg = ((Message) o);
-            Gson gson = new Gson();
-            switch (msg.getHeader("type").toString()) {
-                case "type":
-                    String type = msg.getBody(String.class);
-                    old.setType(type);
-                    break;
-                case "motif":
-                    String motif = msg.getBody(String.class);
-                    old.setMotif(motif);
-                    break;
-                case "price":
-                    String price = msg.getBody(String.class);
-                    old.setPrice(price);
-                    break;
-
             }
         }
     };
 
-    private static AggregationStrategy merge = new AggregationStrategy() {
-        @Override
-        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
-            Message newIn = newExchange.getIn();
-            if (oldExchange == null) {
-                SupportingRequest supportingRequest = new SupportingRequest();
-                add(supportingRequest, newIn);
-                newIn.setBody(supportingRequest);
-                return newExchange;
-            } else {
-                SupportingRequest old = oldExchange.getIn().getBody(SupportingRequest.class);
-                add(old, newIn);
-                oldExchange.getIn().setBody(old);
-                //System.out.println("MERGED " + old);
-                if (old != null &&
-                        old.getLivingExpenses() != null &&
-                        old.getEmployer() != null &&
-                        old.getTravel() != null) {
-                    //old.getFlight() != null) {
-                    System.out.println("EVERYTHING IS AGGREGATED\n" + old);
-                }
-                return oldExchange;
-            }
-        }
-
-        private void add(SupportingRequest old, Object o) {
-            Message msg = ((Message) o);
-            Gson gson = new Gson();
-            switch (msg.getHeader("type").toString()) {
-                case "livingExpenses":
-                    String livingExpenses = msg.getBody(String.class);
-                    old.setLivingExpenses(livingExpenses);
-                    break;
-                case "employer":
-                    String employer = msg.getBody(String.class);
-                    old.setEmployer(employer);
-                    break;
-                case "travel":
-                    String travel = msg.getBody(String.class);
-                    old.setTravel(travel);
-                    break;
-            }
-        }
-
-
-    };
 }
