66680d546eec0d9f210fbb73bea6cd2776e80fe6
fpastor <flopastor06@gmail.com>
2017-11-08 21:58:09 +0100

    exclude integration

--- chaos.sh
+++ chaos.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-tablo=($(docker ps -aq | grep -v $(docker ps -q --filter='name=integration')))
+exclude=$(docker ps -q --filter='name=integration')
+tablo=($(docker ps -aq))
 taille=${#tablo[@]}
 
 while true
 do
     rand=$((RANDOM % $taille))
+    while [ ${tablo[$rand]} -eq $exclude ]
+    do
+        rand=$((RANDOM % $taille))
+    done
+
     docker stop ${tablo[$rand]}
     sleep 10s
     docker start ${tablo[$rand]}
